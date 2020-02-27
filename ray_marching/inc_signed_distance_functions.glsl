5f713404e48ac7877f0eef98a25bf827249abea0
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2020-02-27 09:59:09 +0100

    add SDF triangle

--- ray_marching/inc_signed_distance_functions.glsl
+++ ray_marching/inc_signed_distance_functions.glsl
@@ -202,3 +202,23 @@ float sdPyramid(vec3 cameraPos, vec3 position, float h) {
 
     return sqrt((d2+q.z*q.z)/m2) * sign(max(q.z, -position.y));
 }
+
+float sdTriangle(vec3 cameraPos, vec3 position, vec3 a, vec3 b, vec3 c) {
+    position = distance(cameraPos, position);
+    vec3 ba = b - a; vec3 pa = position - a;
+    vec3 cb = c - b; vec3 pb = position - b;
+    vec3 ac = a - c; vec3 pc = position - c;
+    vec3 nor = cross(ba, ac);
+
+    return sqrt(
+    (sign(dot(cross(ba, nor), pa)) +
+    sign(dot(cross(cb, nor), pb)) +
+    sign(dot(cross(ac, nor), pc))<2.0)
+    ?
+    min(min(
+    dot2(ba*clamp(dot(ba, pa)/dot2(ba), 0.0, 1.0)-pa),
+    dot2(cb*clamp(dot(cb, pb)/dot2(cb), 0.0, 1.0)-pb)),
+    dot2(ac*clamp(dot(ac, pc)/dot2(ac), 0.0, 1.0)-pc))
+    :
+    dot(nor, pa)*dot(nor, pa)/dot2(nor));
+}
