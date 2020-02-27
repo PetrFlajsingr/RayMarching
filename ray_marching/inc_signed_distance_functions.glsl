b39250260e2aa17f570d9de0024c107beab5ec43
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2020-02-27 08:49:44 +0100

    add SDF round box

--- ray_marching/inc_signed_distance_functions.glsl
+++ ray_marching/inc_signed_distance_functions.glsl
@@ -1,3 +1,5 @@
+// https://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
+
 float sdSphere(vec3 pos, vec3 center, float radius) {
     return distance(pos, center) - radius;
 }
@@ -6,4 +8,9 @@ float sdBox(vec3 pos, vec3 start, vec3 dimensions) {
     const vec3 dist = distance(pos, start);
     const vec3 q = abs(dist) - dimensions;
     return length(max(dimensions, 0.0)) + min(max(dimensions.x, max(dimensions.y, dimensions.z)), 0.0);
+}
+
+float sdRoundBox(vec3 pos, vec3 start, vec3 dimensions, float radius) {
+    vec3 q = abs(start) - dimensions;
+    return length(max(q, 0.0)) + min(max(q.x, max(q.y,q.z)), 0.0) - radius;
 }
