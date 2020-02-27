21c021c66178822544fe48b6cccb19fdc80025a3
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2020-02-27 09:20:49 +0100

    add SDF capsule, triprism

--- ray_marching/inc_signed_distance_functions.glsl
+++ ray_marching/inc_signed_distance_functions.glsl
@@ -29,3 +29,26 @@ float sdHexPrism(vec3 cameraPos, vec3 position, vec2 dimensions) {
     position.z-dimensions.y );
     return min(max(d.x, d.y), 0.0) + length(max(d, 0.0));
 }
+
+float sdTriPrism(vec3 cameraPos, vec3 position, vec2 dimensions) {
+    position = distance (cameraPos, position);
+    const float k = sqrt(3.0);
+    dimensions.x *= 0.5 * k;
+    position.xy /= dimensions.x;
+    position.x = abs(position.x) - 1.0;
+    position.y = position.y + 1.0 / k;
+    if(position.x + k * position.y > 0.0){
+        position.xy=vec2(position.x - k * position.y, -k * position.x - position.y) / 2.0;
+    }
+    position.x -= clamp(position.x, -2.0, 0.0);
+    float d1 = length(position.xy) * sign(-position.y) * dimensions.x;
+    float d2 = abs(position.z) - dimensions.y;
+    return length(max(vec2(d1, d2), 0.0)) + min(max(d1, d2), 0.);
+}
+
+float sdCapsule(vec3 cameraPos, vec3 position, vec3 a, vec3 b, float radius) {
+    position = distance(cameraPos, position);
+    const vec3 pa = position - a, ba = b - a;
+    const float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+    return length(pa - ba * h) - radius;
+}
