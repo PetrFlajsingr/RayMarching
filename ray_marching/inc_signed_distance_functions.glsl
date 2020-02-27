474964b10665139c6ec1e025cfb4d862441d1f90
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2020-02-27 08:48:17 +0100

    add SDF box

--- ray_marching/inc_signed_distance_functions.glsl
+++ ray_marching/inc_signed_distance_functions.glsl
@@ -1,4 +1,9 @@
 float sdSphere(vec3 pos, vec3 center, float radius) {
-    return length(pos - center) - radius;
+    return distance(pos, center) - radius;
 }
 
+float sdBox(vec3 pos, vec3 start, vec3 dimensions) {
+    const vec3 dist = distance(pos, start);
+    const vec3 q = abs(dist) - dimensions;
+    return length(max(dimensions, 0.0)) + min(max(dimensions.x, max(dimensions.y, dimensions.z)), 0.0);
+}
