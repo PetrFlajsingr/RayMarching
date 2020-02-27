0fa8151b27127addc911b48c9dc94154fb26e967
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2020-02-27 09:59:54 +0100

    add SDF quad

--- ray_marching/inc_signed_distance_functions.glsl
+++ ray_marching/inc_signed_distance_functions.glsl
@@ -216,9 +216,33 @@ float sdTriangle(vec3 cameraPos, vec3 position, vec3 a, vec3 b, vec3 c) {
     sign(dot(cross(ac, nor), pc))<2.0)
     ?
     min(min(
-    dot2(ba*clamp(dot(ba, pa)/dot2(ba), 0.0, 1.0)-pa),
-    dot2(cb*clamp(dot(cb, pb)/dot2(cb), 0.0, 1.0)-pb)),
-    dot2(ac*clamp(dot(ac, pc)/dot2(ac), 0.0, 1.0)-pc))
+    dot(ba*clamp(dot(ba, pa)/dot(ba), 0.0, 1.0)-pa),
+    dot(cb*clamp(dot(cb, pb)/dot(cb), 0.0, 1.0)-pb)),
+    dot(ac*clamp(dot(ac, pc)/dot(ac), 0.0, 1.0)-pc))
     :
-    dot(nor, pa)*dot(nor, pa)/dot2(nor));
+    dot(nor, pa)*dot(nor, pa)/dot(nor));
+}
+
+float sdQuad(vec3 cameraPos, vec3 position, vec3 a, vec3 b, vec3 c, vec3 d )
+{
+    position = distance(cameraPos, position);
+    vec3 ba = b - a; vec3 pa = position - a;
+    vec3 cb = c - b; vec3 pb = position - b;
+    vec3 dc = d - c; vec3 pc = position - c;
+    vec3 ad = a - d; vec3 pd = position - d;
+    vec3 nor = cross( ba, ad );
+
+    return sqrt(
+    (sign(dot(cross(ba,nor),pa)) +
+    sign(dot(cross(cb,nor),pb)) +
+    sign(dot(cross(dc,nor),pc)) +
+    sign(dot(cross(ad,nor),pd))<3.0)
+    ?
+    min( min( min(
+    dot2(ba*clamp(dot(ba,pa)/dot2(ba),0.0,1.0)-pa),
+    dot2(cb*clamp(dot(cb,pb)/dot2(cb),0.0,1.0)-pb) ),
+    dot2(dc*clamp(dot(dc,pc)/dot2(dc),0.0,1.0)-pc) ),
+    dot2(ad*clamp(dot(ad,pd)/dot2(ad),0.0,1.0)-pd) )
+    :
+    dot(nor,pa)*dot(nor,pa)/dot2(nor) );
 }
