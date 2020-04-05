69b623409d8f958d5281dcefe1fb2eb13886ce3a
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2020-04-05 09:44:40 +0200

    fix info text in gpu panel

--- UI/GPUPanel.cpp
+++ UI/GPUPanel.cpp
@@ -8,7 +8,7 @@ auto ui::GPUPanel::onFrame() -> void {
   if (isVisible()) {
     ImGui::Begin("GPU usage");
     const auto [usedMemory, availableMemory] = getGPUMemoryUsage();
-    ImGui::Text("Memory total: %u\nMemory used: %u", availableMemory, usedMemory);
+    ImGui::Text("Memory total: %u\nMemory unused: %u", availableMemory, usedMemory);
     ImGui::End();
   }
 }
